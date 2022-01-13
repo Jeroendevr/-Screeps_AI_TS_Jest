8cc8cbb422727d48c127ecff5f78aeed41e9c688
Jeroendv <jeroendevries@runbox.eu>
2022-01-13 14:28:20 +0100

    create construction site

--- src/manager/manager.infra.ts
+++ src/manager/manager.infra.ts
@@ -24,20 +24,20 @@ var infraManager = {
 function road_from_control_to_energy(spawns: StructureSpawn): PathStep[] {
     // find energy near spawn
     infraManager.cur_room = spawns.room.name
-    var energy_source = new RoomPosition(39, 44, infraManager.cur_room)
-    return spawns.room.findPath(spawns.pos, energy_source)
+    var energy_source = new RoomPosition(35, 20, infraManager.cur_room)
+    return spawns.room.findPath(spawns.pos, energy_source, { ignoreCreeps: true })
 };
 
 function create_construction_site(path: PathStep,): void {
     //check if its already a construction site
     let pos = path
     const ROOM_POS = new RoomPosition(pos.x, pos.y, infraManager.cur_room)
-
-    // const found = ROOM_POS.lookFor(LOOK_CONSTRUCTION_SITES || LOOK_STRUCTURES)
-    // if (found) {
-    //     console.log(found)
-    // }
-    console.log(infraManager.cur_room, ROOM_POS, pos.x, pos.y)
+    console.log(ROOM_POS)
+    const found = ROOM_POS.lookFor(LOOK_CONSTRUCTION_SITES || LOOK_STRUCTURES)
+    if (found[0] == undefined) {
+        ROOM_POS.createConstructionSite(STRUCTURE_ROAD)
+    }
+    // console.log(infraManager.cur_room, ROOM_POS, pos.x, pos.y)
 }
 
 export {
