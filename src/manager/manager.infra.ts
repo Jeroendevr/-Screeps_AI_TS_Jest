f7cb7927f12294272fbdb625afa8510283733d9d
Jeroendv <jeroendevries@runbox.eu>
2022-01-13 08:38:39 +0100

    create roomPos object. for construction site

--- src/manager/manager.infra.ts
+++ src/manager/manager.infra.ts
@@ -1,12 +1,18 @@
+import { inRange } from "lodash"
 
 var infraManager = {
+    cur_room: "",
 
     run: function runManagerInfra(): void {
         for (const i in Game.spawns) {
-            var spawns = Game.spawns[i]
-            var path = road_from_control_to_energy(spawns)
+            var spawns: StructureSpawn = Game.spawns[i]
+            var path: PathStep[] = road_from_control_to_energy(spawns)
             // Check for construction sites on every PathStep
-
+            for (const j in path) {
+                create_construction_site(path[j])
+                var step = JSON.stringify(path[j])
+                // console.log(step)
+            }
         }
 
     }
@@ -17,12 +23,21 @@ var infraManager = {
 
 function road_from_control_to_energy(spawns: StructureSpawn): PathStep[] {
     // find energy near spawn
-    var energy_source = new RoomPosition(39, 44, spawns.room.name)
+    infraManager.cur_room = spawns.room.name
+    var energy_source = new RoomPosition(39, 44, infraManager.cur_room)
     return spawns.room.findPath(spawns.pos, energy_source)
 };
 
-function create_construction_site(path: PathStep): void {
+function create_construction_site(path: PathStep,): void {
+    //check if its already a construction site
+    let pos = path
+    const ROOM_POS = new RoomPosition(pos.x, pos.y, infraManager.cur_room)
 
+    // const found = ROOM_POS.lookFor(LOOK_CONSTRUCTION_SITES || LOOK_STRUCTURES)
+    // if (found) {
+    //     console.log(found)
+    // }
+    console.log(infraManager.cur_room, ROOM_POS, pos.x, pos.y)
 }
 
 export {
