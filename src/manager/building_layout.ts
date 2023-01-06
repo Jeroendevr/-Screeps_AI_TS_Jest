f5b716ccd61eb639445944033ee9db88b3bbbe1b
Jeroen de Vries <jeroendevriesrunbox.eu>
2023-01-06 14:42:02 +0100

    Comment GCL 2 structures

--- src/manager/building_layout.ts
+++ src/manager/building_layout.ts
@@ -1,7 +1,7 @@
 /**
  * @param pos position of the structure you want to build around for example spawn
  * @returns [x pos , y pos, A structure constant]
- * Return absolute positions based on the passed position. 
+ * Return absolute positions based on the passed position.
  */
 function building_layout(pos: RoomPosition): [number, number, string] {
   let next_structure_site = gcl_lv2_structures(1)!;
@@ -11,6 +11,13 @@ function building_layout(pos: RoomPosition): [number, number, string] {
   return next_structure_site;
 }
 
+/**
+ *
+ * @param nr
+ * @returns
+ * At GCL lvl 2;
+ *   - 5 extensions are possible
+ */
 function gcl_lv2_structures(nr: number) {
   const structure = new Map<number, [number, number, string]>([
     [1, [+1, -1, "extension"]],
