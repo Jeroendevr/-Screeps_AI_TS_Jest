c77d7b422b1fa89adeb96e5b404d6a97de63fa18
Jeroen de Vries <jeroendevriesrunbox.eu>
2023-01-07 20:25:13 +0100

    Check if room position is available for construction

--- src/manager/building_layout.ts
+++ src/manager/building_layout.ts
@@ -26,4 +26,41 @@ function gcl_lv2_structures(nr: number) {
   const arr = structure.get(nr);
   return arr;
 }
-export { building_layout };
+
+function extension_layout(pos: RoomPosition) {
+  while (!gcl_lvl2_extensions().next().done) {
+    let extension_gen = gcl_lvl2_extensions().next().value;
+    extension_gen[0] = extension_gen[0] + pos.x;
+    extension_gen[1] = extension_gen[1] + pos.y;
+    return extension_gen;
+  }
+
+  throw new Error("could not found a new extension layout");
+}
+
+/**
+ *
+ * @returns an array with a relative x and Y position marked for extensions
+ */
+function* gcl_lvl2_extensions() {
+  const extension_locations: [number, number, string][] = [
+    [+1, -1, "extension"],
+    [+2, +0, "extension"]
+  ];
+  const i = 0;
+  while (i < extension_locations.length) {
+    yield extension_locations[i];
+  }
+  return extension_locations[i];
+}
+
+const string_gen = return_string();
+
+const hi: string = string_gen.next().value;
+
+function* return_string() {
+  yield "hello";
+  return "no result";
+}
+
+export { building_layout, extension_layout };
--- src/manager/manager.construction.spec.ts
+++ src/manager/manager.construction.spec.ts
@@ -2,17 +2,11 @@ import { mockGlobal, mockInstanceOf, mockStructure } from "screeps-jest";
 import { constructionManager } from "./constructor";
 import { ConstructionManager } from "./manager.construction";
 
-const ROOM_NAME = "A1A1";
-
-const roomWithoutController = mockInstanceOf<Room>({
-  controller: undefined,
-  name: "A1A1"
-});
-
 const mySpawn = mockStructure(STRUCTURE_SPAWN);
 const mySpawnPos = mockInstanceOf<RoomPosition>({
   x: 10,
-  y: 10
+  y: 10,
+  lookFor: () => [STRUCTURE_SPAWN]
 });
 const myNewConstrSite = mockInstanceOf<RoomPosition>({
   x: 11,
@@ -24,10 +18,31 @@ const myOccupiedConstrSite = mockInstanceOf<RoomPosition>({
   y: 10,
   lookFor: () => [ConstructionSite]
 });
+const suitableConstrSite = mockInstanceOf<RoomPosition>({
+  x: 12,
+  y: 10,
+  lookFor: () => {}
+});
 
-const roomWithSpawn = mockInstanceOf<Room>({});
+const ROOM_NAME = "A1A1";
 
-const MyGame = mockGlobal<Game>("Game", {});
+const roomWithoutController = mockInstanceOf<Room>({
+  controller: undefined,
+  name: ROOM_NAME
+});
+
+const roomWithSpawn = mockInstanceOf<Room>({
+  name: ROOM_NAME,
+  RoomPosition: {
+    mySpawnPos
+  }
+});
+
+const MyGame = mockGlobal<Game>("Game", {
+  rooms: {
+    0: roomWithSpawn
+  }
+});
 
 describe("run", () => {
   it("Does not have a controller", () => {
@@ -50,5 +65,11 @@ describe("run", () => {
   it("Tries to place a stucture on the spawn", () => {
     const CON_MAN = new ConstructionManager(ROOM_NAME);
     expect(CON_MAN.pos_available_construction(myOccupiedConstrSite)).toBeFalsy;
+  });
+  it("Calls a generator function until it finds a good RoomPosition", () => {
+    // MyGame;
+    const CON_MAN = new ConstructionManager(ROOM_NAME);
+    const ROOM_POS = CON_MAN.pos_available_construction(suitableConstrSite);
+    expect(ROOM_POS == true);
   });
 });
